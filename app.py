0baa5129821e82be75c16dc2d6f06c86e0a87d04
cocozxu <73022042+cocozxu@users.noreply.github.com>
2021-05-06 13:50:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,27 @@ def get_assignments():
     assignments = [a.serialize() for a in Assignment.query.all()]
     return success_response(assignments)
 
+#User login endpoint
+@app.route('/login/',methods=["POST"])
+def login():
+    body = json.loads(request.data)
+    email=body.get("email")
+    password=body.get("password")
+    if email is None or password is NOne:
+        return json.dumps({"error": "Invalid email or password"})
+    user=get_user_by_email(email)
+    success=user is not None and user.verigy.password(password)
+    if not sucess:
+        return json.dumps({"error": "Incorrect email or password"})
+    return json.dumps(
+        {
+            "session_token":user.session_token,
+            "session_expiration":str(user.session_expiration)，
+            "update_token":user.update_token,
+        }
+    )
+    
+
 #User register endpoint
 @app.route('/register/',methods=["POST"])
 def register_account():
@@ -80,6 +101,46 @@ def register_account():
             "update_token":user.update_token,
         }
     )
+
+#User session update token endpoint
+@app.route('/session/',methods=["POST"])
+def update_session():
+    sucess, update_token=extract_token(request)
+    
+    if not sucess:
+        return update_token
+    
+    user=get_user_by_update_token(update_token)
+    
+    if user is None:
+        return json.dumps({"error":f"Invalid update token: {update_token}"})
+    user.renew_session()
+    db.session.commit()
+    return json.dumps(
+        {
+            "session_token":user.session_token,
+            "session_expiration":str(user.session_expiration)，
+            "update_token":user.update_token,
+        }
+    )
+
+#User message endpoint
+@app.route('/secret/',methods=["GET"])
+def secret_message():
+    sucess, session_token=extract_token(request)
+    
+    if not sucess:
+        return session_token
+    
+    user=get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return json.dumps({"error": "Invalid session token"})
+    
+    return json.dumps({"message": "You have successfully implemented sessions."})
+                                
+    
+    
+
 
 
 
